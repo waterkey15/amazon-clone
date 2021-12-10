08a88e7c7f75345c531eecabeef347b1fc319fb5
waterkey15 <hdorukkilic@hotmail.com>
2021-12-09 19:59:35 -0500

    header done

---
+++ amazon-clone/src/firebase/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC2B8HNKF3E7gBhCbUKy7GEhTrm_O7XIa8",
+    authDomain: "clone-github-5a1c7.firebaseapp.com",
+    projectId: "clone-github-5a1c7",
+    storageBucket: "clone-github-5a1c7.appspot.com",
+    messagingSenderId: "728648486195",
+    appId: "1:728648486195:web:dd91b8aeb1a3e982198246",
+    measurementId: "${config.measurementId}"
+  };
---
+++ amazon-clone/src/header/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./Header.css"
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+
+
+function Header() {
+    return (
+        <div className="header">
+            <img className='header_logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+            <div className="header_search">
+                <input className="header_searchInput" type="text"/>
+                <SearchIcon className="header_searchIcon"/>
+            </div>
+            <div className="header_nav">
+                <div className="header_option">
+                    <span className="header_optionLineOne">
+                        Hello Guest
+                    </span>
+                    <span className="header_optionsTwo">
+                        Sign In
+                    </span>
+                </div>
+
+                <div className="header_option">
+                    <span className="header_optionLineOne">
+                        Returns
+                    </span>
+                    <span className="header_optionsTwo">
+                        & Orders
+                    </span>
+                </div>
+
+                <div className="header_option">
+                    <span className="header_optionLineOne">
+                        Your
+                    </span>
+                    <span className="header_optionsTwo">
+                        Prime
+                    </span>
+                </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon/>
+                    <span className="header_optionsTwo header_baskCount">
+                        0
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+ 
+export default Header
