2d5d10c5560a74bea14fcd3f3f661d80c158c7fa
waterkey15 <hdorukkilic@hotmail.com>
2021-12-10 18:07:06 -0500

    checkout product added

--- amazon-clone/src/checkout/Checkout.js
+++ amazon-clone/src/checkout/Checkout.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from '../subtotal/Subtotal'
+import CheckoutProduct from '../product/CheckoutProduct'
+import { useStateValue } from '../states/StateProvider'
 
 function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue()
+    
     return (
         <div className="checkout">
             <div className="checkout_left">
@@ -11,7 +16,16 @@ function Checkout() {
                     <h2 className="checkout_title">
                         Your Shopping Basket
                     </h2>
-                    {/* BasketItem */}
+                    {basket.map(item => (
+                        <CheckoutProduct 
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                    ))}
+
                 </div>
             </div>
             <div className="checkout_right">
--- amazon-clone/src/states/reducer.js
+++ amazon-clone/src/states/reducer.js
@@ -8,7 +8,6 @@ function calcTotal(basket){
     for(i; i<basket.length; i++){
         total = total + basket[i].price
     }
-
     return total
 }
 
@@ -25,6 +24,26 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item]
             }
+
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+
+            if(index >=0 ){
+                newBasket.splice(index, 1);
+
+            }else{
+                console.warn('cant remove no such product')
+            }
+
+            return{
+                ...state,
+                basket: newBasket
+            }
+
+
         default:
             return state
     }
